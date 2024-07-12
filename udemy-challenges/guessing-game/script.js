bb10b6670ad65233b3badeb1d2ccc2262202382d
MALIK AAMIR <mehmoodmaamir@gmail.com>
2024-07-12 18:56:27 -0400

    guessing game complete:

--- udemy-challenges/guessing-game/script.js
+++ udemy-challenges/guessing-game/script.js
@@ -1,40 +1,51 @@
-const answer = Math.trunc(Math.random() * 20) + 1
-console.log(answer)
+let answer = Math.trunc(Math.random() * 20) + 1
 
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+  document.querySelector(".message").textContent = message;
+}
+
+const displayScore = function(score){
+  document.querySelector(".score").textContent = score;
+}
 
 document.querySelector(".check").addEventListener('click', function(){
   const guess = Number(document.querySelector(".guess").value);
 
   if (guess === answer) {
     document.querySelector("body").style.backgroundColor = '#60b347';
-    document.querySelector(".message").textContent = "Correct number";
+    displayMessage("Correct number");
     document.querySelector(".number").style.width = '30rem';
     document.querySelector(".number").textContent = answer
-  } else if (guess > answer) {
+
+    if(score > highscore){
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore
+    }
+  } else if (guess !== answer) {
     if (score > 0){
-      document.querySelector(".message").textContent = "📈 Too high! Go low";
+      displayMessage(guess > answer ? "📈 Too high!" : "📉 Too low!");
       score--;
-      document.querySelector(".score").textContent = score;
+      displayScore(score);
     }else{
-      document.querySelector(".message").textContent = "You lost the game.. :( ";
+      displayMessage("You lost the game.. :( ");
     }
-  } else if (guess < answer) {
-    if (score > 0) {
-      document.querySelector(".message").textContent = "📉 Too low! Go high";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game.. :( ";
-    }
-  } else if (!guess) {
-    document.querySelector(".message").textContent = "Enter a number between 1 and 20";
+  }  else if (!guess) {
+    displayMessage("Enter a number between 1 and 20");
   }
 })
 
 document.querySelector(".again").addEventListener('click', function(){
-  let score = 20;
-  document.querySelector(".message").textContent = "Start guessing";
+  score = 20;
+  displayScore(score);
+
+  answer = Math.trunc(Math.random() * 20) + 1
+
+  displayMessage("Start guessing...");
   document.querySelector(".number").style.width = '15rem';
   document.querySelector("body").style.backgroundColor = '#222';
+  document.querySelector(".number").textContent = '?';
+  document.querySelector(".guess").value = "";
 })
