f71c264230ba7dd9044dce55fb1b62ef2c593af1
MALIK AAMIR <mehmoodmaamir@gmail.com>
2025-03-21 15:20:25 -0400

    finished fundamentals review

--- udemy-challenges/fundamentals_review/script.js
+++ udemy-challenges/fundamentals_review/script.js
@@ -364,4 +364,32 @@ for (let i = 0; i < time.length; i++){
   console.log(hours, mints);
   console.log(`It's been ${hours} hours and ${mints} minutes.`);
 }
+
 */
+
+// WHILE LOOPS
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function(bill){
+  return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
+
+for (let i=0; i <bills.length; i++){
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
+}
+console.log(tips, totals);
+
+let sum = 0;
+
+const calcAverage = function(arr){
+  for (let i=0; i < arr.length; i++){
+    sum += arr[i]
+  }
+}
+
+calcAverage([1, 2, 3])
+console.log(sum);
